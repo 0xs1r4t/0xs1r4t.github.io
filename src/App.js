060d80ec6ab4faec354ea2be30e5c4f6c3b323b7
sBx99 <sirat.b99@gmail.com>
2021-01-23 01:56:35 +0530

    new style (dark theme only)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react'
 
+// components
 import GlobalStyle from './components/GlobalStyle'
-import Header from './components/Header'
-import Description from './components/Description'
-import Links from './components/Links'
+
+// assets
+import darkThemeGif from './assets/dark-theme.gif';
 
 function App() {
   return (
     <div>
       <GlobalStyle />
-      <Header />
-      <br />
-      <Description />
-      <br />
-      <Links />
+      <img src={darkThemeGif} alt="new website who dis 👀" />
+      <h1>find me on <a href="https://sirat.xyz" className="highlight" target="_blank" rel="noopener noreferrer">sirat.xyz</a></h1>
     </div>
   )
 }
--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,120 +1,65 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,700;1,400;1,700&display=swap');
-
-    @font-face {
-        font-family: 'Sporting Grotesque Regular';
-        src: url('https://cdn.glitch.com/4e08fdce-4f09-4168-8b21-d44271860232%2FSportingGrotesque-Regular.otf?v=1599669222539') format("otf"),
-            url('https://cdn.glitch.com/4e08fdce-4f09-4168-8b21-d44271860232%2FSportingGrotesque-Regular.woff?v=1599671067024') format('woff'),
-            url('https://cdn.glitch.com/4e08fdce-4f09-4168-8b21-d44271860232%2FSportingGrotesque-Regular.woff2?v=1599671069801') format('woff2');
-        font-style: normal;
-        font-display: swap;
-    }
-
-    @font-face {
-        font-family: 'Sporting Grotesque Bold';
-        src: url('https://cdn.glitch.com/4e08fdce-4f09-4168-8b21-d44271860232%2FSportingGrotesque-Bold.otf?v=1599669221861') format("otf"),
-            url('https://cdn.glitch.com/4e08fdce-4f09-4168-8b21-d44271860232%2FSportingGrotesque-Bold.woff?v=1599671065908') format('woff'),
-            url('https://cdn.glitch.com/4e08fdce-4f09-4168-8b21-d44271860232%2FSportingGrotesque-Bold.woff2?v=1599671066327') format('woff2');
-        font-style: normal;
-        font-display: swap;
-          }
-
-    @font-face {
-        font-family: 'AUTHENTIC Sans';
-        src: url("https://cdn.glitch.com/4e08fdce-4f09-4168-8b21-d44271860232%2FAUTHENTICSans-90.woff2?v=1595102445095") format("woff2"),
-            url("https://cdn.glitch.com/4e08fdce-4f09-4168-8b21-d44271860232%2FAUTHENTICSans-90.woff?v=1595102445203") format("woff"),
-            url("https://cdn.glitch.com/4e08fdce-4f09-4168-8b21-d44271860232%2FAUTHENTICSans-90.otf?v=1595102498458") format("otf");
-        font-style: normal;
-        font-display: swap;
-      }
-
-    /* variable names will be changed later */
-
     * {
-  	    --white: white;
-  	    --black: black;
-	      --soft-pink: #f0c9f4;
-  	    --soft-purple: #CEB3FE;
-  	    --bright-blue: #396FFD;
-  	    --bright-pink: #FF69B4;
-	      --bright-purple: #8651d3;
-  	    --soft-green: #BEFEB3;
-	      --distance: 0.5em;
-	      --large: 2.25em;
+      --black: #0F0F0F;
+      --white: #F0F0F0;
+      --pink: #E838FF;
+      --purple: #6F38FF;
     }
 
     html {
-  	    display: flex;
-	      flex-direction: column;
-  	    margin: 0;
-  	    padding: 0;
-  	    align-items: center;
-  	    justify-content: center;
-	      height: 100vh;
+        margin: 0;
+        padding: 0;
+        height: 100%
     }
 
     body {
+        display: flex;
+	      flex-direction: column;
+  	    align-items: center;
+  	    justify-content: center;
+        height: 100vh;
+
   	    text-align: center;
-  	    font-family: 'AUTHENTIC Sans', 'Space Mono', monospace;
-  	    background: var(--bright-purple);
-  	    color: var(--soft-green);
+  	    font-family: monospace;
+  	    background: var(--black);
+  	    color: var(--white);
 	      max-width: 100vw;
     }
 
-    .mid {
-        display: flex;
-	      flex-direction: column;
-	      max-width: 650px;
-	      align-items: center;
-	      align-content: center;
-	      justify-items: center;
-        justify-content: center;
-        margin: 0;
-        padding: 0;
+    h1 {
+      font-family: Cardo, serif;
+      color: var(--purple);
+      font-weight: 600;
+      font-size: 1.5em;
+      letter-spacing: 0.05em;
     }
 
-    .links {
-        margin-top: -0.25em;
-        font-size: var(--large);
-        flex-direction: row;
+    .highlight {
+      color: var(--pink);
+      text-decoration: none;
+      border-bottom: 2px solid var(--purple);
     }
 
-    a {
-        font-weight: bold;
-        color: var(--soft-green);
-        text-decoration: none;
-    }
-
-    a:hover {
-        color: var(--soft-pink);
-    }
-
-    ::-moz-selection {
-      background-color: var(--bright-blue);
-      color: var(--soft-purple);
+    .highlight:hover {
+      background: rgba(111, 56, 255, 0.5);
+      border-bottom: 0px;
     }
 
     ::selection {
-      background-color: var(--bright-blue);
-      color: var(--soft-purple);
+      background-color: var(--pink);
+      color: var(--white);
     }
 
-    /* a rainbow animation i might use later*/
-    @keyframes RainbowAnim {
-      0%,
-      100% {
-        background-image: linear-gradient(to right, pink, yellow, cyan, violet);
-      }
-      25% {
-        background-image: linear-gradient(to right, violet, pink, yellow, cyan);
-      }
-      50% {
-        background-image: linear-gradient(to right, cyan, violet, pink, yellow);
-      }
-      75% {
-        background-image: linear-gradient(to right, yellow, cyan, violet, pink);
+    ::-moz-selection {
+      background-color: var(--pink);
+      color: var(--white);
+    }
+
+    @media screen and (max-width: 600px) {
+        img {
+          width: 95vw;
       }
     }
 `
